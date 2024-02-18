c38a0aa9e7339d69a788e6348a75f48798d53c4e
Kauan Sena <lkrs@protonmail.com>
2024-02-18 16:03:58 -0300

    Update generate password function

--- api/pypass.py
+++ api/pypass.py
@@ -4,5 +4,5 @@ import string
 
 def generate_password(num_char):
     char = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(char) for _ in range(num_char))
+    password = ''.join(random.choice(char) for _ in range(int(num_char)))
     return password
