a6756f040c0e06cf4a6cd469b6882300702b5357
Kauan Sena <lkrs@protonmail.com>
2024-03-02 17:20:29 -0300

    CLI Application
    Run ipassword on bash or CMD with python and one single line command

---
+++ CLI/ipassword.py
@@ -0,0 +1,19 @@
+import random
+import string
+import argparse
+
+parser = argparse.ArgumentParser(prog='https://ipassword.vercel.app/.py', description='Password Generator')
+
+parser.add_argument('--num','-n', dest='num', help='Specifies the number of characters in the password')
+
+args = parser.parse_args()
+
+def generate_password(num_char):
+    char = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(char) for _ in range(int(num_char)))
+    return password
+
+length = args.num
+new_password = generate_password(length)
+print(f"\n\033[1;0;0mPASSWORD: \033[1;33m{new_password}\033[0;0;0m\n")
+
